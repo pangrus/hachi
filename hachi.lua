89da9c2c8aedc64a1bbb702638f2f2da8e507e02
Pangrus <pangrus@gmail.com>
2020-11-16 15:04:33 +0100

    Update hachi.lua

--- hachi.lua
+++ hachi.lua
@@ -105,6 +105,9 @@ function init_patterns()
         }
         generate_pattern(i)
     end
+        -- default rotation parameter for hh and snare
+        instrument[2].r = 2
+        instrument[3].r = 4
 end
 
 function clear_patterns()
@@ -118,11 +121,10 @@ function clear_patterns()
             rot = {}
         }
         generate_pattern(i)
-        
-        -- default rotation paremeter for hh and snare
+    end
+     -- default rotation parameter for hh and snare
         instrument[2].r = 2
         instrument[3].r = 4
-    end
 end
 
 function randomize_pattern(sel)
